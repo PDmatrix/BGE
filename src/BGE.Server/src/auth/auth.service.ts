63fc651a0931630101ae636983eb1234199a6c4b
PDmatrix <dimasebakovgoo@gmail.com>
2019-07-10 14:45:49 +0300

    JWT Payload as interface

--- src/BGE.Server/src/auth/auth.service.ts
+++ src/BGE.Server/src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
 
-  public async getToken(userId: string) {
-    return await this.jwtService.signAsync({ sub: () => userId });
+  public getToken(userId: string) {
+    const user: JwtPayload = {
+      sub: userId,
+    };
+    return this.jwtService.signAsync(user);
   }
 }
