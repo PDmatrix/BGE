f62e6426143df11d0de23661673ef92d02da0b07
PDmatrix <dimasebakovgoo@gmail.com>
2019-07-25 07:56:53 +0300

    Accept game handler refactoring

--- src/BGE.Server/src/api/commands/handlers/accept-game.handler.ts
+++ src/BGE.Server/src/api/commands/handlers/accept-game.handler.ts
@@ -3,7 +3,8 @@ import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { AuthService } from '../../../auth/auth.service';
 import { EngineService } from '../../engine.service';
 import { GameAcceptedEvent } from '../../events/impl/game-accepted.event';
-import { GameStatus } from '../../interfaces/game-state.interface';
+import { GameStatus, IGameState } from '../../interfaces/game-state.interface';
+import { IPlayerState } from '../../interfaces/player-state.interface';
 import { GameStateRepository } from '../../repositories/game-state.repository';
 import { PlayerStateRepository } from '../../repositories/player-state.repository';
 import { AcceptGameCommand } from '../impl/accept-game.command';
@@ -18,14 +19,29 @@ export class AcceptGameHandler implements ICommandHandler<AcceptGameCommand> {
     private readonly authService: AuthService,
   ) {}
 
-  async execute(command: AcceptGameCommand) {
-    const { userId, gameToken } = command;
+  public async execute({ userId, gameToken }: AcceptGameCommand) {
+    const gameState = await this.getGameStateByToken(gameToken);
+    await this.createGame(gameState, userId);
+    await this.updateOpponentId(gameState.userTurnId, userId);
+    await this.updateGameStatus(gameState._id);
+    this.eventBus.publish(new GameAcceptedEvent(gameState.userTurnId));
+    const userToken = await this.authService.getToken(userId);
+    return { userToken, gameToken };
+  }
 
+  private async getGameStateByToken(gameToken: string) {
     const gameState = await this.gameStateRepository.findByGameToken(gameToken);
     if (!gameState) {
       throw new BadRequestException('Provided gameToken is invalid');
     }
 
+    return gameState;
+  }
+
+  private async createGame(
+    gameState: IGameState,
+    userId: string,
+  ): Promise<void> {
     const startResponse = await this.engineService.startGame(
       gameState.rows,
       gameState.cols,
@@ -37,17 +53,22 @@ export class AcceptGameHandler implements ICommandHandler<AcceptGameCommand> {
       opponentId: gameState.userTurnId,
       userId,
     });
+  }
 
-    await this.playerStateRepository.updateOneByUserId(gameState.userTurnId, {
-      opponentId: userId,
+  private updateOpponentId(
+    userId: string,
+    opponentId: string,
+  ): Promise<IPlayerState> {
+    return this.playerStateRepository.updateOneByUserId(userId, {
+      opponentId,
     });
+  }
 
-    await this.gameStateRepository.updateOneById(gameState._id, {
+  private updateGameStatus(
+    gameStateId: string | undefined,
+  ): Promise<IGameState> {
+    return this.gameStateRepository.updateOneById(gameStateId, {
       status: GameStatus.Playing,
     });
-
-    this.eventBus.publish(new GameAcceptedEvent(gameState.userTurnId));
-    const userToken = await this.authService.getToken(userId);
-    return { userToken, gameToken };
   }
 }
